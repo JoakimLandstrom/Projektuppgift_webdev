611cb0ab897207bf59d3882aa31a264203487886
JoakimLandstrom <jockez@live.com>
2016-01-18 13:25:34 +0100

    structure

---
+++ src/se/webdev/ju15/Controller/Controller.java
@@ -0,0 +1,12 @@
+package se.webdev.ju15.Controller;
+
+import javax.servlet.http.HttpServlet;
+
+public class Controller extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/se/webdev/ju15/dB/DataBase.java
@@ -0,0 +1,5 @@
+package se.webdev.ju15.dB;
+
+public class DataBase {
+
+}
